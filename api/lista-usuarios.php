af88ccc1d19d58460f54f0841f2f504d638a1c53
ER1CK122 <erckgames53@gmail.com>
2022-12-14 14:34:17 -0300

    lista-usuarios feita com sucesso

---
+++ api/lista-usuarios.php
@@ -0,0 +1,16 @@
+<?php
+  header("content-type: application/json");
+  header("Access-Control-Allow-Origin: *");
+
+  require_once('../public/configs/conexaoSql.php');
+
+  $sql = "SELECT * FROM usuarios";
+  $res = mysqli_query($conn, $sql) or die(' Erro na query:'. $sql . '' . mysqli_error($conn));
+
+  if (mysqli_num_rows($res) > 0) {
+    $output = mysqli_fetch_all($res, MYSQLI_ASSOC);
+    echo json_encode($output);
+  } else {
+    echo json_encode(array("message"=> "No Records Found","status"=> FALSE));
+  }
+?>
